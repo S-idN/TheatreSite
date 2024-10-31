da5c6ababc3e0c87ceee26cc28427e5627757d8e
S-idN <siddharth2004nair@gmail.com>
2024-10-31 18:44:28 +0530

    Added login page frontend

--- TheaterappDjang/settings.py
+++ TheaterappDjang/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'Theaterapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'my-app/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,38 +1,19 @@
-import Header from "./components/Header"
-import MainBody from "./components/MainBody"
-import {Clist} from "./components/Data"
-import { MdChevronLeft, MdChevronRight } from "react-icons/md"
-import Footer from "./components/Footer"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {HomePage} from "./Pages/HomePage";
+import { LoginPage } from "./Pages/LoginPage";
+import { Test } from "./Pages/Test";
 
 export default function App() {
 
-  const SlideLeft = ()=>{
-    var slider = document.getElementById('slider')
-    slider!.scrollLeft = slider!.scrollLeft - 950
-  }
-
-
-  const SlideRight = ()=>{
-    var slider = document.getElementById('slider')
-      slider!.scrollLeft = slider!.scrollLeft + 950
-  }
-
-  return (
-    <div className="bg-gradient-to-r from-red-950 via-indigo-950 to-neutral-800 z-0 flex flex-col justify-center">
-      <Header/>
-      <div className="relative flex item-center ">
-        <MdChevronLeft className="self-center opacity-50 hover:opacity-100" onClick={SlideLeft} size={40} color="pink"/>
-        <div id="slider" className="scrollbar-hide w-full h-full overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth">
-          {Clist.map((item)=> (
-            <img className="w-[1080px] inline-block p-4 hover:scale-105 ease-in-out duration-500" 
-            src={import.meta.env.BASE_URL + item.image} 
-            alt="cover images" />
-          ))}
-        </div>
-        <MdChevronRight className="self-center opacity-50 hover:opacity-100" onClick={SlideRight} size={40} color="pink"/>
-      </div>
-      <MainBody/>
-      <Footer/>
-    </div>
-  )
+    return(
+      <>
+        <Router>
+          <Routes>
+            <Route path="test" element={<Test/>}/>
+            <Route path="login" element={<LoginPage/>}/>
+            <Route path="" element={<HomePage/>} />
+          </Routes>
+        </Router>
+      </>
+    )
 }
